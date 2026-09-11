1223b693cc24f1ca6d1b8cfea22b25810250bfe5
Mykhailo Zhar <mykhailo.zhar.od.ua@gmail.com>
2026-09-11 13:21:47 +0300

    Add pid to search

--- src/search.py
+++ src/search.py
@@ -1,27 +1,35 @@
 import logging
+import os
 from pathlib import Path
 
 
 def search(file_path: Path, word: str, logger: logging.Logger) -> bool:
+    pid = os.getpid()
     try:
         with file_path.open() as file:
-            logger.debug("Starting search for %s in %s", word, file_path.name)
+            logger.debug(
+                "PID(%s) Starting search for %s in %s", pid, word, file_path.name
+            )
             file_content = file.read()
             words = file_content.split()
 
             for file_word in words:
                 if file_word.lower() == word:
-                    logger.debug("Search ended successfully %s", file_path.name)
+                    logger.debug(
+                        "PID(%s) Search ended successfully %s", pid, file_path.name
+                    )
                     return True
 
     except FileNotFoundError as e:
-        logger.error(f"Error: Could not find file - {e.filename}")
+        logger.error("PID(%s) Error: Could not find file - %s", pid, e.filename)
     except PermissionError as e:
         logger.error(
-            f"Unable to read file. Insufficient permissions - {e.filename}",
+            "PID(%s) Unable to read file. Insufficient permissions - %s",
+            pid,
+            e.filename,
         )
     except OSError as e:
-        logger.error(f"Other error: - {e}")
+        logger.error("PID(%s) Other error: - %s", pid, e)
 
-    logger.debug("Search ended %s", file_path.name)
+    logger.debug("PID(%s) Search ended %s", pid, file_path.name)
     return False
