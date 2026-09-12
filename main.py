4a45f76450fc7d82acefe5c35aac7a72cd000a53
Mykhailo Zhar <mykhailo.zhar.od.ua@gmail.com>
2026-09-12 19:21:18 +0300

    Add time measurement

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+import logging
+import timeit
+from functools import partial
+
+from src.concurrent_search import search_concurrent
+from src.parallel_search import search_parallel
+from src.utility import configure_logger, get_files
+
+ITERATIONS = 100
+
+
 def main():
-    print("Hello from TODO: replace goit-pycore-template!")
+    logger = logging.getLogger("Time logger")
+    configure_logger(logger, prefix="time")
+
+    files = get_files()
+    keywords = ["she", "like"]
+    quiet_logger = logging.getLogger("None")
+
+    search_funcs = [search_parallel, search_concurrent]
+
+    for search_fn in search_funcs:
+        elapsed = timeit.timeit(
+            partial(search_fn, files, keywords, quiet_logger),
+            number=ITERATIONS,
+        )
+        logger.info(
+            "%s: %.4f s total, %.4f s/iter (%d iterations)",
+            search_fn.__name__,
+            elapsed,
+            elapsed / ITERATIONS,
+            ITERATIONS,
+        )
 
 
 if __name__ == "__main__":
